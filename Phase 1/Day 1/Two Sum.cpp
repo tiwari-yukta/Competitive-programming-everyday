1534bf26cacf95e7fca307c16f4bfd6c1c4c4ec0
Yukta Tiwari <136195552+tiwari-yukta@users.noreply.github.com>
2025-01-10 14:40:40 +0530

    Update Two Sum.cpp

--- Phase 1/Day 1/Two Sum.cpp
+++ Phase 1/Day 1/Two Sum.cpp
@@ -1,1 +1,22 @@
-Leetcode 
+// Leetcode -> https://leetcode.com/problems/two-sum
+
+  // My Code->
+      vector<int> o=twoSum(vector<int>&arr, int target){
+        unordered_map<int,int>mp;
+        vector<int>ans(2,0);
+
+        for(int it=0;it<arr.size();it++){
+            mp[arr[it]]=it;
+        } 
+        for(int i=0;i<arr.size();i++){
+            int rem=target-arr[i];
+
+            if(mp.find(rem)!=mp.end() && mp[rem]!=i){
+                ans[0]=i;
+                ans[1]=mp[rem];
+                break;
+            }
+            
+        }
+        return ans;
+    }
